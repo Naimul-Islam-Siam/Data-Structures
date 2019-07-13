d910c74837cb65a60d22128038604e3b7ed7b258
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-07-14 01:35:04 +0600

    set, insert and remove method added

--- 2. Doubly Linked List/doubly linked list.js
+++ 2. Doubly Linked List/doubly linked list.js
@@ -106,6 +106,7 @@ class DoublyLinkedList {
         if (index <= this.length / 2) {
             currentNode = this.head;
             counter = 0;
+
             while (counter !== index) {
                 currentNode = currentNode.next;
                 counter++
@@ -122,6 +123,53 @@ class DoublyLinkedList {
             }
         }
 
-        return currentNode.val;
+        return currentNode;
+    }
+
+    set(index, val) {
+        const foundNode = this.get(index);
+
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        } else return false;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(val);
+        if (index === this.length) return !!this.push(val);
+
+        const newNode = new Node(val);
+        let beforeNode = this.get(index - 1);
+        let afterNode = beforeNode.next;
+
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        afterNode.prev = newNode;
+
+        this.length++;
+
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        let targetNode = this.get(index);
+        let beforeNode = targetNode.prev;
+        let afterNode = targetNode.next;
+
+        beforeNode.next = afterNode;
+        afterNode.prev = beforeNode;
+        targetNode.next = null;
+        targetNode.prev = null;
+
+        this.length--;
+
+        return this;
     }
 };
