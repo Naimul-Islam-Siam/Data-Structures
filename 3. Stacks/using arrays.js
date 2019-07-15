c7522e8ecf2ed5dda2cdf27b8098ad31c7d29b1e
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-07-15 18:00:22 +0600

    stack using array

---
+++ 3. Stacks/using arrays.js
@@ -0,0 +1,13 @@
+//array implementation
+let stack = [];
+//push
+stack.push("First");
+stack.push("Second");
+stack.push("Third");
+console.log(stack);
+
+//pop
+stack.pop();
+console.log(stack);
+stack.pop();
+console.log(stack);
