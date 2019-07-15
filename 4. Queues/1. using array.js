c02ad0a10de9c243051139631e43ddd98bb175e5
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-07-15 17:55:24 +0600

    queue using array

---
+++ 4. Queues/1. using array.js
@@ -0,0 +1,12 @@
+//array implementation
+let queue = [];
+//enqueue
+queue.unshift("First");
+queue.unshift("Second");
+queue.unshift("Third");
+console.log(queue);
+//dequeue
+queue.pop();
+console.log(queue);
+queue.pop();
+console.log(queue);
